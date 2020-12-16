6a8dddf647a3e5a2a8b103de16df97b46b95ce63
Andrea <dauriaandrea99@gmail.com>
2020-12-16 10:43:20 +0100

    2° BONUS ! END !

--- bonus/app.js
+++ bonus/app.js
@@ -9,6 +9,20 @@ let vm = new Vue({
             "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
         ]
     },
+    //BONUS
+    created: function() {
+        setInterval(this.next,3000);
+    },
+    //BONUS
+    mounted() {
+        document.addEventListener("keyup",e =>{
+            if(e.key === "ArrowRight") {
+                this.next();
+            } else if (e.key === "ArrowLeft"){
+                this.prev();
+            }
+        });
+    },
     methods: {
         next(){
             this.counter ++;
@@ -22,6 +36,7 @@ let vm = new Vue({
                 this.counter = this.images.length -1;
             }
         },
+        //BONUS
         clickCircle(index){
             this.counter = index;
         }
